77875c450a7541b83968dccab707b1284c2ba906
KamilKhairullin <kamil.khayrullin.personal@yandex.ru>
2021-08-29 22:26:40 +0300

    test

--- app_python/myapp/tests/test_main.py
+++ app_python/myapp/tests/test_main.py
@@ -10,17 +10,3 @@ def client():
 def test_acess(client):
     r = requests.get("http://127.0.0.1:5000")
     assert r.status_code == 200, "Unreached."
-
-
-# class FlaskrTestCase(unittest.TestCase):
-#     def setUp(self):
-#         pass
-#     def tearDown(self):
-#         pass
-
-#     def request_test(self):
-#         #r = requests.get("http://127.0.0.1:5000")
-#         assert False#r.status_code == 200, "Unreached."
-
-# #if __name__ == '__main__':
-# unittest.main()
