f177b76cfa521f76a1836a9090bbf938f11cdbb1
KamilKhairullin <kamil.khayrullin.personal@yandex.ru>
2021-08-31 20:19:32 +0300

    workflow

--- app_python/myapp/tests/test_main.py
+++ app_python/myapp/tests/test_main.py
@@ -1,13 +1,11 @@
+import requests
+import pytest
+import unittest
 
-# import requests
-# import pytest
-# import unittest
+@pytest.fixture
+def client():
+    pass
 
-# @pytest.fixture
-# def client():
-#     pass
-
-# def test_acess(client):
-#     r = requests.get("http://127.0.0.1:5000")
-#     assert r.status_code == 200, "Unreached."
-pass
+def test_acess(client):
+    r = requests.get("http://127.0.0.1:5000")
+    assert r.status_code == 200, "Unreached."
